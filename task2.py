cbd14925da8887aaf8a5a7fad03e9fd192760612
Nafisa Ali <93398098+naffisa@users.noreply.github.com>
2021-11-02 20:16:10 +0000

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,17 @@
+mynumber = '1,100'
+message = input('what is your favourite numer?')
+print("What do dentists call an astronaut's cavity?")
+msg = input()
+print('black whole') 
+
+
+message = input('what is your favourite numer?')
+print('What do you get when you cross a snowman with a vampire?')
+name = input()
+print('Frostbite!')
+
+
+message = input('what is your favourite numer?')
+print('what is the difference between a good joke and a bad joke?')
+name = input()
+print('timing')
---
+++ task3.py
@@ -0,0 +1,8 @@
+food = 'favourite meal'
+message1 = input('what is your favourite meal?')
+print(message1)
+message2 = input('what is your favourite drink?')
+print(message2)
+message3 = input('what is your favourite desset?')
+print(message3)
+print('your favourite meal is' , message1 , 'with a glass of', message2)
