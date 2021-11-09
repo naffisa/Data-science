4e5c59f4a36d3f361221e4df294ea9a10efef473
Nafisa Ali <93398098+naffisa@users.noreply.github.com>
2021-11-09 20:11:34 +0000

    Add files via upload

---
+++ Numpy1.py
@@ -0,0 +1,12 @@
+number = []
+
+print('Enter 4 numbers')
+
+numbers.append(input('1:'))
+numbers.append(input('2:'))
+numbers.append(input('3:'))
+numbers.append(input('4:'))
+
+file = open('numbers.txt', 'w')
+for number in numbers:
+    file.write(number+"\n")
